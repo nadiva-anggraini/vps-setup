030a61fb8c73840c5e24aca6910671a3da0aff93
Nadiva Anggraini <119149860+nadiva-anggraini@users.noreply.github.com>
2024-10-31 22:21:26 +0700

    Update vps-setup.sh

--- vps-setup.sh
+++ vps-setup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 sudo apt update
-
 echo "Membuka port SSH"
 sudo ufw allow ssh
 sudo ufw enable
 echo "Port SSH telah dibuka."
-
 echo "Install Node js...."
 apt install zip
 sudo apt install htop
@@ -14,33 +12,24 @@ source ~/.bashrc
 fnm use --install-if-missing 22.11.0
 sudo apt install screen
 sudo apt install git
-
 echo "Install Docker Engine...."
 sudo apt-get update
 sudo apt-get install ca-certificates curl
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
 echo "Install Python...."
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
 sudo apt install python3.13
 sudo ln -sf /usr/bin/python3 /usr/bin/python
-
 echo "Install Rivalz Node...."
 npm i -g rivalz-node-cli
-
-
 echo "Install Nillion Verifier...."
 docker pull nillion/verifier:v1.0.1
-
 echo "Installing Dawn Internet Bot...."
 git clone https://github.com/nadiva-anggraini/dawn-bot
 cd dawn-bot
