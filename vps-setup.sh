ab5fef32850f7736d60841381bd6bcbb3289849e
Nadiva Anggraini <119149860+nadiva-anggraini@users.noreply.github.com>
2024-10-31 22:19:08 +0700

    Update vps-setup.sh

--- vps-setup.sh
+++ vps-setup.sh
@@ -1,38 +1,20 @@
 #!/bin/bash
-
-# Perbarui package manager
 sudo apt update
 
-# Buka port SSH
 echo "Membuka port SSH"
 sudo ufw allow ssh
 sudo ufw enable
 echo "Port SSH telah dibuka."
 
-# Install Node js
 echo "Install Node js...."
 apt install zip
 sudo apt install htop
 curl -fsSL https://fnm.vercel.app/install | bash
 source ~/.bashrc
 fnm use --install-if-missing 22.11.0
+sudo apt install screen
+sudo apt install git
 
-# Periksa apakah Screen terinstal, jika belum instal Screen
-if ! command -v screen &> /dev/null; then
-    echo "Screen tidak ditemukan, menginstal Screen..."
-    sudo apt install screen || { echo "Gagal menginstal Screen. Keluar."; exit 1; }
-else
-    echo "Screen sudah terinstal."
-fi
-
-# Periksa apakah Git terinstal, jika belum instal Git
-if ! command -v git &> /dev/null; then
-    echo "Git tidak ditemukan, menginstal Git..."
-    sudo apt install git || { echo "Gagal menginstal Git. Keluar."; exit 1; }
-else
-    echo "Git sudah terinstal."
-fi
-# Install Docker Engine
 echo "Install Docker Engine...."
 sudo apt-get update
 sudo apt-get install ca-certificates curl
@@ -45,7 +27,6 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Install Python
 echo "Install Python...."
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
@@ -53,16 +34,13 @@ sudo apt update
 sudo apt install python3.13
 sudo ln -sf /usr/bin/python3 /usr/bin/python
 
-
-# Install Rivalz Node
 echo "Install Rivalz Node...."
 npm i -g rivalz-node-cli
 
-# Install Nillion Verifier
+
 echo "Install Nillion Verifier...."
 docker pull nillion/verifier:v1.0.1
 
-# Install Dawn Internet Bot
 echo "Installing Dawn Internet Bot...."
 git clone https://github.com/nadiva-anggraini/dawn-bot
 cd dawn-bot
